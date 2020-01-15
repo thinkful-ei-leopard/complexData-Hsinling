9093ae2009f6143a4c3efe10133efe6060e32371
Hsin Ling Hu <hsinkava11@gmail.com>
2020-01-15 13:56:16 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+'use strict';
+
+// put your own value below!
+const apiKey = 'AIzaSyD65Q_E9IYY2tfrd75ACtXj7j3soRddq0g'; 
+const searchURL = 'https://www.googleapis.com/youtube/v3/search';
+
+
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+  return queryItems.join('&');
+}
+
+
+function getYouTubeVideos(query, maxResults=10) {
+  const params = {
+    key: apiKey,
+    q: query,
+    part: 'snippet',
+    maxResults
+  };
+  const queryString = formatQueryParams(params)
+  const url = searchURL + '?' + queryString;
+
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => console.log(JSON.stringify(responseJson)))
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
+}
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    const searchTerm = $('#js-search-term').val();
+    const maxResults = $('#js-max-results').val();
+    getYouTubeVideos(searchTerm, maxResults);
+  });
+}
+
+$(watchForm);
