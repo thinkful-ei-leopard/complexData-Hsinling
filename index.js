94a489d3e1378d998888a49f0ca94ebeeb196446
Hsin Ling Hu <hsinkava11@gmail.com>
2020-01-15 23:00:17 -0800

    revised index.html and index.js

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
-'use strict';
+
 
 // put your own value below!
-const apiKey = 'AIzaSyD65Q_E9IYY2tfrd75ACtXj7j3soRddq0g'; 
-const searchURL = 'https://www.googleapis.com/youtube/v3/search';
-
+const apiKey = '4Q6I0BZRnkEBb6jDP1vwrPed96KVyxyDDWg2CToo'; 
+const searchURL = 'https://developer.nps.gov/api/v1/park';
+//Query String Parameters - parkCode=acad,dena
 
 function formatQueryParams(params) {
   const queryItems = Object.keys(params)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
   return queryItems.join('&');
 }
 
+function finalResult(responseJson){  
+  console.log(responseJson);
+  let inputhtml = '';
+  for(let i=0; i<responseJson.data.length; i++){
+    const park = responseJson.data[i];
+    const name = park.fullName;
+    const description = park.description;
+    const webUrl = park.url;
 
-function getYouTubeVideos(query, maxResults=10) {
+  
+    inputhtml += `<li>
+  <p>Name:${name}</p>
+  <p>Description:${description}</p>
+  <p>Website URL:${webUrl}</p>
+ </li>`;
+  } 
+  $('#results-list').html(inputhtml);
+  $('#results').removeClass('hidden');
+}
+
+function getNationalPark(query, maxResults=10) {
   const params = {
     key: apiKey,
-    q: query,
-    part: 'snippet',
-    maxResults
+    stateCode: query,
+    limit:maxResults
   };
-  const queryString = formatQueryParams(params)
+  const queryString = formatQueryParams(params);
   const url = searchURL + '?' + queryString;
 
   fetch(url)
@@ -29,7 +47,7 @@ function getYouTubeVideos(query, maxResults=10) {
       }
       throw new Error(response.statusText);
     })
-    .then(responseJson => console.log(JSON.stringify(responseJson)))
+    .then(responseJson => finalResult(responseJson))
     .catch(err => {
       $('#js-error-message').text(`Something went wrong: ${err.message}`);
     });
@@ -38,9 +56,10 @@ function getYouTubeVideos(query, maxResults=10) {
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
-    const searchTerm = $('#js-search-term').val();
-    const maxResults = $('#js-max-results').val();
-    getYouTubeVideos(searchTerm, maxResults);
+    $('#result-list').empty();
+    const state = $('input[type="text"]').val();
+    const maxResults = $('input[type="number"]').val();
+    getNationalPark(state, maxResults);
   });
 }
 
